dd28a2bfdcea7b2a025c9953fe6cac404b56cb29
UlyanovskiyDmitry <131540820+UlyanovskiyDmitry@users.noreply.github.com>
2025-12-14 23:15:43 +0300

    Add simpleStorage wwev app

---
+++ storage.js
@@ -0,0 +1,49 @@
+// Адрес вашего развернутого контракта
+    const contractAddress = '0x9fCfc62e24Ebf3eAd3326B0aa870776F99DDC9A4';
+    // ABI вашего контракта (можно взять тот набор, который формирует Remix)
+    const contractAbi = [
+	    {
+		    "inputs": [
+			    {
+				    "internalType": "string",
+				    "name": "_message",
+				    "type": "string"
+			    }
+		    ],
+		    "name": "setMessage",
+		    "outputs": [],
+		    "stateMutability": "nonpayable",
+		    "type": "function"
+	    },
+	    {
+		    "inputs": [],
+		    "name": "getMessage",
+		    "outputs": [
+			    {
+				    "internalType": "string",
+				    "name": "",
+				    "type": "string"
+			    }
+		    ],
+		    "stateMutability": "view",
+		    "type": "function"
+        }
+    ];
+
+    // При подключении к Mmask
+    if (window.ethereum) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(contractAddress, contractAbi, signer);
+        document.getElementById('setMessageButton').onclick = async () => {
+            const message = document.getElementById('messageInput').value;
+            await contract.setMessage(message);
+            alert('Сообщение установлено!');
+        };
+        document.getElementById('getMessageButton').onclick = async () => {
+            const message = await contract.getMessage();
+            document.getElementById('messageDisplay').innerText = message;
+        };
+    } else {
+        alert('Установите MMask или другой кошелек.');
+    }
